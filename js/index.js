bb59f0de1dc63709d84776575340eea7cf64f65a
TI Sabbir <tisabbir0@gmail.com>
2024-03-03 16:17:24 +0600

    now after clicking on  the message button, the title and the view counts are shown in the title section, the mark as read number is incrased

--- js/index.js
+++ js/index.js
@@ -23,7 +23,7 @@ const loadPosts = () => {
     
                         <div class="space-y-4">
                             <p>#<span>${post.category}</span> <span class="ml-5">Author : </span> <span>${post.author.name}</span></p>
-                            <h1 class="text-[#12132D] text-xl font-bold">${post.title}</h1>
+                            <h1 id="dis-title" class="text-[#12132D] text-xl font-bold">${post.title}</h1>
                             <p class="para text-left">${post.description}</p>
                             <hr class="border-dashed bg-black">
                             <div class="flex justify-between items-center"> 
@@ -31,13 +31,13 @@ const loadPosts = () => {
                                     <i class="fa-regular fa-message"></i>
                                 <p>${post.comment_count}</p>
                                 <i class="fa-regular fa-eye"></i>
-                                <p>${post.view_count}</p>
+                                <p id="dis-views">${post.view_count}</p>
                                 <i class="fa-regular fa-clock"></i>
                                 <p>${post.posted_time}</p>
                                 </div>
-                                <div class="btn bg-green-400 p-2 rounded-full">
-                                    <i class="fa-solid fa-square-envelope w-8"></i>
-                                </div>
+                    
+                                    <i onclick="msgBtnHandler()" class="fa-solid fa-square-envelope w-8 btn bg-green-400 p-2 rounded-full"></i>
+                                
                             </div>
                         </div>
                     </div>
@@ -72,4 +72,33 @@ function checkActive(posts) {
         a++;
     }
   })
+}
+
+const titleSection = document.getElementById('title-container');
+let count = 0;
+const markAsRead = document.getElementById('mark-as-read');
+
+const msgBtnHandler=()=>{
+    const card = event.target.parentNode.parentNode;
+    const title = card.childNodes[3].innerText;
+    const views = event.target.parentNode.childNodes[1].childNodes[7].innerText
+
+
+    const titles = document.createElement('div');
+
+    titles.innerHTML = `
+    <div class="title-container flex items-center bg-white rounded-2xl p-4 mt-4 justify-between">
+                        <h1>${title}</h1>
+                        <div class="flex gap-2 items-center">
+                            <i class="fa-regular fa-eye"></i>
+                            <p>${views}</p>
+                        </div>
+                    </div>
+    `
+    titleSection.appendChild(titles);
+
+
+    count++;
+    markAsRead.innerText = count;
+    
 }
