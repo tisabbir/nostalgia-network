bcd03a38fac654b42bc852a2aa2346205440c685
TI Sabbir <tisabbir0@gmail.com>
2024-03-03 13:21:22 +0600

    green red btn problem solved Alhamdulillah

--- js/index.js
+++ js/index.js
@@ -1,33 +1,39 @@
-const loadPosts = async () => {
-    const response = await fetch('https://openapi.programming-hero.com/api/retro-forum/posts')
-    const data = await response.json();
-    const posts = data.posts;
-    
-    const discussionContainer = document.getElementById('discussion-container');
+let posts;
+const getData = async (loadPosts, checkActive) => {
+  const response = await fetch(
+    "https://openapi.programming-hero.com/api/retro-forum/posts"
+  );
+  const data = await response.json();
+  posts = data.posts;
+  loadPosts(posts);
+  checkActive(posts);
+};
 
-    posts.forEach((post)=>{
-        let discussion = document.createElement('div');
-        
-        discussion.innerHTML = `
+const loadPosts = () => {
+  const discussionContainer = document.getElementById("discussion-container");
+  posts.forEach((post) => {
+    let discussion = document.createElement("div");
+
+    discussion.innerHTML = `
         <div id="discussion" class="flex gap-6 bg-[#F3F3F5] p-3 lg:p-10 rounded-xl">
 
-                        <div id="profile-picture" class="relative"><img class="w-20 rounded-xl" src="https://i.ibb.co/pjM6Ybr/pexels-anastasia-shuraeva-9607206.jpg" alt="">
-                            <i id="active-status" class="absolute -top-2 -right-2 fa-regular fa-circle bg-green-600 rounded-full"></i>
+                        <div id="profile-picture" class="relative"><img class="w-20 rounded-xl" src="${post.image}" alt="">
+                            <i id="active-status" class="absolute active-status -top-2 -right-2 fa-regular fa-circle rounded-full"></i>
                         </div>
     
                         <div class="space-y-4">
-                            <p>#<span>Coding</span> <span class="ml-5">Author : </span> <span>Safiya Chowdury</span></p>
-                            <h1 class="text-[#12132D] text-xl font-bold">10 Kids Unaware of Their Halloween Costume</h1>
-                            <p class="para text-left">It's one thing to subject yourself to ha Halloween costume mishap because, hey that's your prerogative</p>
+                            <p>#<span>${post.category}</span> <span class="ml-5">Author : </span> <span>${post.author.name}</span></p>
+                            <h1 class="text-[#12132D] text-xl font-bold">${post.title}</h1>
+                            <p class="para text-left">${post.description}</p>
                             <hr class="border-dashed bg-black">
                             <div class="flex justify-between items-center"> 
                                 <div class="flex gap-4 items-center">
                                     <i class="fa-regular fa-message"></i>
-                                <p>560</p>
+                                <p>${post.comment_count}</p>
                                 <i class="fa-regular fa-eye"></i>
-                                <p>1568</p>
+                                <p>${post.view_count}</p>
                                 <i class="fa-regular fa-clock"></i>
-                                <p>5 min</p>
+                                <p>${post.posted_time}</p>
                                 </div>
                                 <div class="btn bg-green-400 p-2 rounded-full">
                                     <i class="fa-solid fa-square-envelope w-8"></i>
@@ -35,10 +41,35 @@ const loadPosts = async () => {
                             </div>
                         </div>
                     </div>
-        `
+        `;
 
-        discussionContainer.appendChild(discussion);
-    })
+    discussionContainer.appendChild(discussion);
+  });
+};
+
+getData(loadPosts, checkActive);
+
+function checkActive(posts) {
+  const discussion = document.querySelectorAll("#discussion");
+  console.log(discussion, "discussion");
+  let activeBtns = [];
+  discussion.forEach((item) => {
+    console.log(item.querySelector("#active-status"));
+    let activeBtn = item.querySelector("#active-status");
+    activeBtns.push(activeBtn);  
+  });
+  console.log(activeBtns)
+
+  let a = 0;
+  posts.forEach(post => {
+    if(post.isActive){
+       console.log(activeBtns[a], 'true');
+
+       activeBtns[a].classList.add('bg-green-400')
+       a++;
+    } else{
+        activeBtns[a].classList.add('bg-red-400')
+        a++;
+    }
+  })
 }
-
-loadPosts();
